55241aabf519e8a0e85737a1b7ce482c67b9c4c4
Unknown <lucafloreau@akeonet.com>
2018-11-05 22:56:38 +0100

    Recipe + Drop

--- Assets/Script/Container.cs
+++ Assets/Script/Container.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Container : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public List<string> currentContainObjectType;
+    private GameObject gameManager;
+
+    // Use this for initialization
+    void Start ()
+    {
+        currentContainObjectType = new List<string>();
+        gameManager = GameObject.FindGameObjectWithTag("GameManager");
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void OnDropObject(List<string> objectType)
+    {
+        currentContainObjectType.AddRange(objectType);
+        Debug.Log("Drop");
+    }
+
 }
---
+++ Assets/Script/Ingredient.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredient : Draggable {
+
+    // Use this for initialization
+    protected override void Start ()
+	{
+        base.Start();
+	    containerType = "Cauldron";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void Drop()
+    {
+        Destroy(gameObject);
+    }
+}
