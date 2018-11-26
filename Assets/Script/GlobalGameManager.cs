a3487afca7b7374698b6ea121306a137dc62ed3d
FloreauLuca <lucafloreau@akeonet.com>
2018-11-27 00:20:03 +0100

    merde

--- Assets/Script/GlobalGameManager.cs
+++ Assets/Script/GlobalGameManager.cs
@@ -86,7 +86,7 @@ public class GlobalGameManager : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
-        EditorApplication.isPlaying = false;
+        //EditorApplication.isPlaying = false;
     }
 
     void ApplyOptions()
